def32c00817c3e36e077945103714bf146e05c70
Kaka <taurakaka04@gmail.com>
2026-05-12 15:16:17 +0700

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/v1/notes'; 
+const API_URL = 'https://notes-be-764024000152.us-central1.run.app/api/v1/notes'; 
 
 const noteTitle = document.getElementById('noteTitle');
 const noteDesc = document.getElementById('noteDesc');
